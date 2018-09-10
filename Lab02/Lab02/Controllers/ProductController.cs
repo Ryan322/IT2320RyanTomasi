e413f1ef5763aeccda4d6eb68209f586dce3717f
Ryan Tomasi <S01051500@acad.tri-c.edu>
2018-09-09 22:00:49 -0400

    Finish

---
+++ Lab02/Lab02/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab02.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Browse()
+        {
+            ViewBag.Message = "Browse displayed.";
+
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Location displayed for zip=44124.";
+
+            return View();
+        }
+
+        //     public string Index()
+        //     {
+        //        return "Product/Index is displayed.";
+        //   }
+
+    }
+}
