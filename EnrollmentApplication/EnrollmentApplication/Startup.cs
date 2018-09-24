ad22196083051b5d8b5bd76764beb51e470e3028
Ryan Tomasi <S01051500@acad.tri-c.edu>
2018-09-23 23:07:56 -0400

    enrollment

---
+++ EnrollmentApplication/EnrollmentApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnrollmentApplication.Startup))]
+namespace EnrollmentApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
