3625765a5d1651d2bb6a66217bd04242d683e7ee
Ryan Tomasi <S01051500@acad.tri-c.edu>
2018-10-21 22:30:57 -0400

    1

---
+++ WebApp_NoAuth/WebApp_NoAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp_NoAuth.Startup))]
+namespace WebApp_NoAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WebIndividual/WebIndividual/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebIndividual.MvcApplication" Language="C#" %>
