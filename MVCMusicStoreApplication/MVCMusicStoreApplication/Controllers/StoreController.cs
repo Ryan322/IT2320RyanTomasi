9e48269e45bffe52b5126e3fc42a520d2008fc9f
Ryan Tomasi <S01051500@acad.tri-c.edu>
2018-11-04 22:54:30 -0500

    L

---
+++ MVCMusicStoreApplication/MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+
+        MVCMusicStoreApplicationDB db = new MVCMusicStoreApplicationDB();
+
+        [HttpGet]
+        public ActionResult Browse()
+        {
+            var Genres = from m in db.Genres;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            int Album = from m in db.Album
+                        where Genreid =("");
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Details()
+        {
+            int Album = from m in db.Album
+                        where Albumid = ("");
+            return View();
+        }
+    }
+}
